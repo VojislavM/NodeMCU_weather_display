dc71da8ff46c8816f11f70eefb0a65f1594d9b31
VojislavM <milivojevicvoja@yahoo.com>
2017-03-26 21:56:22 +0200

    first commit

---
+++ weather_station_firmware/stationCredentials.h
@@ -0,0 +1,11 @@
+/* WIFI */
+const char* WIFI_SSID = "Tenda";
+const char* WIFI_PWD = "video-teka-021";
+
+/* Wunderground Settings */
+const boolean IS_METRIC = true;
+const String WUNDERGRROUND_API_KEY = "fd25b0ebcd3a1793";
+const String WUNDERGRROUND_LANGUAGE = "EN";
+
+const String WUNDERGROUND_COUNTRY = "RS";
+const String WUNDERGROUND_CITY = "Novi_Sad";
