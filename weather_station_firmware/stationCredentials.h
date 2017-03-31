7141e1ceaba1a3f85e8530659cd22c952606708b
VojislavM <milivojevicvoja@yahoo.com>
2017-03-31 16:15:44 +0200

    add schematics picture

--- weather_station_firmware/stationCredentials.h
+++ weather_station_firmware/stationCredentials.h
@@ -1,11 +1,11 @@
 /* WIFI */
-const char* WIFI_SSID = "xxx";
-const char* WIFI_PWD = "xxx";
+const char* WIFI_SSID = "Tenda";
+const char* WIFI_PWD = "video-teka-021";
 
 /* Wunderground Settings */
 const boolean IS_METRIC = true;
-const String WUNDERGRROUND_API_KEY = "xxx";
+const String WUNDERGRROUND_API_KEY = "fd25b0ebcd3a1793";
 const String WUNDERGRROUND_LANGUAGE = "EN";
 
-const String WUNDERGROUND_COUNTRY = "xx";
-const String WUNDERGROUND_CITY = "xxx";
+const String WUNDERGROUND_COUNTRY = "RS";
+const String WUNDERGROUND_CITY = "Novi_Sad";
